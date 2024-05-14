0c8b8f6179289513768e24fff1477a18387f33a4
Ganthswea <133676498+Ganthswea@users.noreply.github.com>
2024-05-14 14:46:41 +0530

    Update config.py

--- config.py
+++ config.py
@@ -32,7 +32,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/Ganthswea/yuri",
+    "https://telegra.ph/file/0b25aa6c39c494091c272.jpg",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 GIT_TOKEN = getenv(
